5b38b58b8e2dae3bcd7e8092e832cd09e1e19a13
Miguel Angel Castillo <321angelcastillo.mc@gmail.com>
2016-12-12 12:12:34 -0800

    I added the drawsquare and drawcircle functions

--- project7.js
+++ project7.js
@@ -13,8 +13,23 @@ function transformPoint(event) {
 // Step 2: drawSquare and drawCircle functions
 function drawSquare(x, y, size, color) {
   // square drawing code here
+  var newsquare = document.createElementNS(namespace, "rect")
+  newsquare.setAttribute("x", xpos)
+  newsquare.setAttribute("y", ypos)
+  newsquare.setAttribute("width", size)
+  newsquare.setAttribute("height", size)
+  newsquare.setAttribute("fill", color);
+  screen.appendChild(newsquare)
 }
 
+function drawCircle(cx, cy, radius, color) {
+  var newcircle = document.createElementNS(namespace, "circle")
+  newcircle.setAttribute("cx", cx)
+  newcircle.setAttribute("cy", cy)
+  newcircle.setAttribute("r", radius)
+  newcircle.setAttribute("fill", color)
+  screen.appendChild(newcircle)
+}
 // Step 3: Event listeners
 document.addEventListener("mousedown", function(e) {
   // what do you want to do when the user presses down
